743b735f59367f3e5f163fd2d18400827026f7b8
Chely Ho <adept_inept@yahoo.com>
2018-07-14 22:59:46 +0000

    automatically backed up by learn

--- app/workers/file_worker.rb
+++ app/workers/file_worker.rb
@@ -3,6 +3,8 @@ class FileWorker
   include Sidekiq::Worker
 
   def perform(songs_file)
-
+    CSV.foreach(songs_file, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
   end
 end
